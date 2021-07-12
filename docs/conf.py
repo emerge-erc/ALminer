698a3211108983d5755e7b7b6fa2c6714883d1ff
Aida Ahmadi <aida.ahmadi67@gmail.com>
2021-07-13 01:55:40 +0200

    Recreating the docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,32 +30,25 @@ release = '0.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.imgmath',
-    'nbsphinx', 'sphinx.ext.mathjax'
-]
-
+extensions = ['nbsphinx']
 source_suffix = ['.rst', '.ipynb']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
-add_function_parentheses = True
 nbsphinx_execute = 'never'
+latex_engine = 'xelatex'
+latex_elements = {'extraclassoptions': 'openany,oneside'}
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-
+#
 html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
