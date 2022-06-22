bb83d4c94577c0565914f5fa0aba8aef4a689ae6
Aida Ahmadi <aida.ahmadi67@gmail.com>
2022-06-22 15:08:49 +0200

    Setting pyvo version to 1.2.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     license='MIT',
-    install_requires=['numpy>=1.15', 'pandas>1.0', 'matplotlib>=3.3.0', 'pyvo==1.3',
+    install_requires=['numpy>=1.15', 'pandas>1.0', 'matplotlib>=3.3.0', 'pyvo==1.2.1',
                       'astropy>=3.1.2', 'astroquery>=0.4.2'],
     python_requires='>=3.6'
 )
